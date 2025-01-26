b64dc20c1d2ff88e217ac9a34cb1407e726128e2
yseto <yseto@users.noreply.github.com>
2025-01-26 01:04:26 +0000

    go run -mod=mod ./cmd/migration/main.go entries_new

---
+++ ent/migrate/migrations/20250126010352_entries_new.sql
@@ -0,0 +1,14 @@
+-- Disable the enforcement of foreign-keys constraints
+PRAGMA foreign_keys = off;
+-- Create "new_entries" table
+CREATE TABLE `new_entries` (`id` integer NOT NULL PRIMARY KEY AUTOINCREMENT, `title` text NOT NULL, `description` text NOT NULL, `url` text NOT NULL, `published_at` datetime NULL, `new` bool NOT NULL DEFAULT (true), `feeds_entries` integer NULL, CONSTRAINT `entries_feeds_entries` FOREIGN KEY (`feeds_entries`) REFERENCES `feeds` (`id`) ON DELETE SET NULL);
+-- Copy rows from old table "entries" to new temporary table "new_entries"
+INSERT INTO `new_entries` (`id`, `title`, `description`, `url`, `published_at`, `feeds_entries`) SELECT `id`, `title`, `description`, `url`, `published_at`, `feeds_entries` FROM `entries`;
+-- Drop "entries" table after copying rows
+DROP TABLE `entries`;
+-- Rename temporary table "new_entries" to "entries"
+ALTER TABLE `new_entries` RENAME TO `entries`;
+-- Create index "entries_url_feeds_entries" to table: "entries"
+CREATE UNIQUE INDEX `entries_url_feeds_entries` ON `entries` (`url`, `feeds_entries`);
+-- Enable back the enforcement of foreign-keys constraints
+PRAGMA foreign_keys = on;
